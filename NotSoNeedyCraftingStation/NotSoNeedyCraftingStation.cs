bc8ae345cdd183d26da34e448905f6a1f8bd84c4
Joel Mclean <joel@oliapps.co.uk>
2021-03-03 21:59:51 +0000

    Initial commit

---
+++ NotSoNeedyCraftingStation/NotSoNeedyCraftingStation.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NotSoNeedyCraftingStation
+{
+    [BepInPlugin("uk.co.oliapps.valheim.notsoneedycraftingstation", "Not So Needy Crafting Station", "0.0.1")]
+    public class NotSoNeedyCraftingStation : BaseUnityPlugin
+    {
+        public void Awake() => Harmony.CreateAndPatchAll(typeof(NotSoNeedyCraftingStation), null);
+
+        [HarmonyPatch(typeof(CraftingStation), "CheckUsable")]
+        [HarmonyPrefix]
+        [HarmonyPriority(Priority.Last)]
+        public static bool CheckUsable(ref CraftingStation __instance, Player player, bool showMessage, ref bool __result)
+        {
+            bool haveFire = (bool)AccessTools.Field(typeof(CraftingStation), "m_haveFire").GetValue(__instance);
+            if (!__instance.m_craftRequireFire || haveFire)
+            {
+                __result = true;
+                return false;
+            }
+            if (showMessage)
+                player.Message(MessageHud.MessageType.Center, "$msg_needfire", 0, (Sprite)null);
+            __result = false;
+            return false;
+        }
+    }
+}
